05535331c7a06c55fe3297e378ebf916b2505331
Marcos Duarte <marcosduarte1994@gmail.com>
2023-03-29 21:09:25 -0300

    feat: create table of users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,88 @@
+import { Sidebar } from '@/components/Sidebar'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tr,
+  Th,
+  Checkbox,
+  Thead,
+  Tbody,
+  Td,
+  Text,
+} from '@chakra-ui/react'
+import { RiAddLine, RiEditLine } from 'react-icons/ri'
+import { Header } from '../../components/Header'
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px={6}>
+        <Sidebar />
+
+        <Box flex={1} borderRadius={8} bg="gray.800" p={8}>
+          <Flex mb={8} justifyContent="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize={20} />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px={6} color="gray.300" width={8}>
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data Cadastro</Th>
+                <Th width={8}></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px={6}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Marcos Duarte</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      marcosduarte1994@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>28 de Março, 2023</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiEditLine} fontSize={20} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
